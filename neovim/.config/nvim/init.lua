4da24c6fee6576da0193ec6f38c9395fdf7c04db
Mike Deakin <madeakin93@gmail.com>
2022-10-03 12:19:32 +0100

    [nvim] Migrate from vim-plug to packer.nvim

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -21,61 +21,10 @@ vim.cmd([[filetype plugin indent on]]) -- auto-indent based on file type
 vim.cmd([[syntax on]])
 vim.opt.completeopt = "menu,menuone,noselect"
 
--- vim-plug plugins
-local Plug = vim.fn['plug#']
-vim.call('plug#begin')
--- Meta
-Plug 'dstein64/vim-startuptime'
-Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
-
--- Style
-Plug 'ryanoasis/vim-devicons'
-Plug 'navarasu/onedark.nvim'
-Plug 'nvim-lualine/lualine.nvim'
-
--- file browser
-Plug('scrooloose/nerdtree', { ['on'] = 'NERDTreeToggle' })
-Plug('xuyuanp/nerdtree-git-plugin', { ['on'] = 'NERDTreetoggle' })
-
--- remote pairing
-Plug('jbyuki/instant.nvim', { ['on'] = { 'InstantStartSession', 'InstantJoinSession' } })
-
--- Additional functions
-Plug 'tpope/vim-sensible'
-Plug 'tpope/vim-sleuth' -- auto indent width detection
-Plug 'tpope/vim-surround' -- surround text with matching character pairs ()[]{}, etc
-Plug 'tpope/vim-abolish' -- word-related mutations (case, endings, search/replace, etc
-Plug 'chaoren/vim-wordmotion' -- camel-case word motions
-Plug 'lukas-reineke/indent-blankline.nvim' -- Show indentation levels
-Plug 'scrooloose/nerdcommenter'
-Plug 'ggandor/leap.nvim' -- label-based navigation (like vimium)
-
--- IDEA-like stuff
-Plug 'neovim/nvim-lspconfig' -- https://github.com/neovim/nvim-lspconfig
-Plug 'weilbith/nvim-code-action-menu' -- lsp code actions in descriptive window
-Plug 'nvim-lua/plenary.nvim' -- required for telescope
-Plug('nvim-telescope/telescope-fzf-native.nvim', { ['do'] = 'make' }) -- fzf-style matching for telescope
-Plug 'nvim-telescope/telescope.nvim' -- Fuzzy finder
-Plug 'nvim-treesitter/nvim-treesitter-textobjects' -- Syntax-aware motions
-
-Plug 'Olical/conjure'
-
--- Completions
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'hrsh7th/cmp-buffer'
-Plug 'hrsh7th/cmp-path'
-Plug 'hrsh7th/cmp-cmdline'
-Plug 'hrsh7th/nvim-cmp'
-Plug 'hrsh7th/cmp-vsnip'
-Plug 'hrsh7th/vim-vsnip'
-
-Plug 'airblade/vim-gitgutter'
-
-vim.call('plug#end')
-
 vim.g.NERDTreeShowHidden = 1
 vim.g.instant_username = 'mdeakin'
 
+require'plugins'
 require'theme'
 require'keymap'
 require'lsp' -- Language Server Protocol
---
+++ neovim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,65 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  -- Meta
+  use 'wbthomason/packer.nvim'
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+
+  -- Style
+  use 'ryanoasis/vim-devicons'
+  use 'navarasu/onedark.nvim'
+  use 'nvim-lualine/lualine.nvim'
+
+  -- file browser
+  use { 'scrooloose/nerdtree', opt = true, cmd = { 'NERDTreeToggle' } }
+  use { 'xuyuanp/nerdtree-git-plugin', opt = true, cmd = { 'NERDTreeToggle' } }
+
+  -- remote pairing
+  use { 'jbyuki/instant.nvim', opt = true, cmd = { 'InstantStartSession', 'InstantJoinSession' } }
+
+  -- Additional functions
+  use 'tpope/vim-sensible'
+  use 'tpope/vim-sleuth' -- auto indent width detection
+  use 'tpope/vim-surround' -- surround text with matching character pairs ()[]{}, etc
+  use 'tpope/vim-abolish' -- word-related mutations (case, endings, search/replace, etc
+  use 'chaoren/vim-wordmotion' -- camel-case word motions
+  use 'lukas-reineke/indent-blankline.nvim' -- Show indentation levels
+  use 'scrooloose/nerdcommenter'
+  use 'ggandor/leap.nvim' -- label-based navigation (like vimium)
+
+  -- IDEA-like stuff
+  use 'neovim/nvim-lspconfig' -- https://github.com/neovim/nvim-lspconfig
+  use 'weilbith/nvim-code-action-menu' -- lsp code actions in descriptive window
+  use 'nvim-lua/plenary.nvim' -- required for telescope
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' } -- fzf-style matching for telescope
+  use 'nvim-telescope/telescope.nvim' -- Fuzzy finder
+  use { 'nvim-treesitter/nvim-treesitter-textobjects', requires = { 'nvim-treesitter/nvim-treesitter' } } -- Syntax-aware motions
+
+  use 'Olical/conjure'
+
+  -- Completions
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+
+  use 'airblade/vim-gitgutter'
+
+  if packer_bootstrap then
+    require'packer'.sync()
+  end
+end)
