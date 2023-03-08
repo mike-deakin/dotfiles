b3e9f4765fddca606e115be947d72c01b70726ca
Mike Deakin <madeakin93@gmail.com>
2023-03-08 13:07:33 +0000

    [nvim] Some nice cmp mappings

--- neovim/.config/nvim/lua/cmp-config.lua
+++ neovim/.config/nvim/lua/cmp-config.lua
@@ -1,11 +1,6 @@
 local cmp = require 'cmp'
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local feedkey = function(key, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
@@ -30,20 +25,29 @@ if cmp ~= nil then
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<C-Space>'] = cmp.mapping.confirm({ select = true }),
-      ["<Tab>"] = cmp.mapping(function(fallback)
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-s>'] = cmp.mapping.complete({
+        config = {
+          sources = {
+            { name = 'vsnip' }
+          }
+        }
+      }),
+      ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
-          cmp.select_next_item()
+          local entry = cmp.get_selected_entry()
+          if not entry then
+            cmp.select_next_item()
+          else
+            cmp.confirm()
+          end
         elseif vim.fn["vsnip#available"](1) == 1 then
           feedkey("<Plug>(vsnip-expand-or-jump)", "")
-        elseif has_words_before() then
-          cmp.complete()
         else
           fallback()
         end
       end, { "i", "s" }),
-
-      ["<S-Tab>"] = cmp.mapping(function()
+      ['<S-Tab>'] = cmp.mapping(function()
         if cmp.visible() then
           cmp.select_prev_item()
         elseif vim.fn["vsnip#jumpable"](-1) == 1 then
@@ -65,7 +69,6 @@ if cmp ~= nil then
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
       { name = 'path' },
-      { name = 'fuzzy_path' },
     }, {
       { name = 'cmdline' }
     })
