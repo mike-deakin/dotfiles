3a33455afd204e23e4d6feca3459f25492953428
Mike Deakin <madeakin93@gmail.com>
2024-12-24 12:12:55 +0000

    [nvim] Fix "Term" command failing if buf deleted

--- neovim/.config/nvim/lua/terminal.lua
+++ neovim/.config/nvim/lua/terminal.lua
@@ -2,8 +2,12 @@ local M = {}
 
 TerminalBuffer = -1
 
+M.bufexists = function(buf)
+  return vim.fn.bufexists(buf) == 1
+end
+
 M.start_or_open_terminal = function()
-  if TerminalBuffer == -1 then
+  if TerminalBuffer == -1 or not M.bufexists(TerminalBuffer) then
     vim.cmd[[botright split]]
     vim.cmd[[terminal]]
     TerminalBuffer = vim.fn.bufnr()
