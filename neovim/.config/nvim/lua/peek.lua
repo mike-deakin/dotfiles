97338b3d2bf478fcbc260f4bf338fc6b3e75d9f6
Mike Deakin <madeakin93@gmail.com>
2025-01-13 11:47:07 +0000

    [nvim] peek.lua API updates

--- neovim/.config/nvim/lua/peek.lua
+++ neovim/.config/nvim/lua/peek.lua
@@ -3,10 +3,10 @@ local function preview_location_callback(_, result, method)
     vim.lsp.log.info(method, 'No location found')
     return nil
   end
-  if vim.tbl_islist(result) then
-    vim.lsp.util.preview_location(result[1])
+  if vim.islist(result) then
+    vim.lsp.util.preview_location(result[1], {})
   else
-    vim.lsp.util.preview_location(result)
+    vim.lsp.util.preview_location(result, {})
   end
 end
 
