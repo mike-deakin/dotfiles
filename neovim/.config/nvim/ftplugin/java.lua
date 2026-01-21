27f877e481425271cd8a5ce21479e5afdd0bc4eb
Mike Deakin <madeakin93@gmail.com>
2026-01-21 10:36:56 +0000

    [nvim] Make java style and imports align better with IntelliJ IDEA

--- neovim/.config/nvim/ftplugin/java.lua
+++ neovim/.config/nvim/ftplugin/java.lua
@@ -4,37 +4,32 @@ local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 local workspace_dir = vim.env.HOME .. '/.cache/jdtls/' .. project_name
 
 require('jdtls').start_or_attach({
-    cmd = {
-        vim.env.HOME .. '/.config/lsp/jdtls/bin/jdtls',
-        '--jvm-arg=-javaagent:' .. vim.env.HOME .. '/.config/libs/lombok.jar',
-        '-configuration ' .. vim.env.HOME .. '/.cache/jdtls',
-        '-data', workspace_dir,
-    },
-    on_attach = lsp_config.on_attach,
-    settings = {
-        java = {
-            signatureHelp = { enabled = true },
-            completion = {
-                importOrder = { -- Import order roughly matches IntelliJ ordering
-                    "",         -- "All other packages"
-                    "java",
-                    "javax",
-                    "#", -- "All static imports"
-                }
-            },
-            --configuration = {
-            --runtimes = {
-            --{
-            --name = 'JavaSE-17',
-            --path = '~/.sdkman/candidates/java/17.0.2-oracle'
-            --}
-            --},
-            --maven = {
-            --downloadSources = true,
-            --}
-            --},
+  cmd = {
+    vim.env.HOME .. '/.config/lsp/jdtls/bin/jdtls',
+    '--jvm-arg=-javaagent:' .. vim.env.HOME .. '/.config/libs/lombok.jar',
+    '-configuration ' .. vim.env.HOME .. '/.cache/jdtls',
+    '-data', workspace_dir,
+  },
+  on_attach = lsp_config.on_attach,
+  settings = {
+    java = {
+      signatureHelp = { enabled = true },
+      format = {
+        settings = {
+          url = '~/.config/nvim/jdtls_style/java.xml',
+          profile = 'GoogleStyle'
+        },
+      },
+      completion = {
+        importOrder = { -- Import order roughly matches IntelliJ ordering
+          "",           -- "All other packages"
+          "java",
+          "javax",
+          "#", -- "All static imports"
         }
+      },
     }
+  }
 })
 
 --require 'neotest'.setup({
